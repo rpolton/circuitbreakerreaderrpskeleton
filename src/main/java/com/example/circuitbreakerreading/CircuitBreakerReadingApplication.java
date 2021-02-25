bf8f00c4b183f880067d64b5df2f11f227b4016d
rpolton <richaerd>
2021-02-25 14:09:05 +0000

    add URL so that when we're demo'ing the source is clear

--- src/main/java/com/example/circuitbreakerreading/CircuitBreakerReadingApplication.java
+++ src/main/java/com/example/circuitbreakerreading/CircuitBreakerReadingApplication.java
@@ -1,5 +1,8 @@
 package com.example.circuitbreakerreading;
 
+// Check this code out from the github repository
+// https://github.com/rpolton/circuitbreakerreaderrpskeleton
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
